1ea76672a06d0d6f1463c4a417c088bba8e60cea
Peter Brian Gwadenya <75813303+pgwiz@users.noreply.github.com>
2024-10-07 22:29:50 +0300

    Update settings.py

--- ctrack/settings.py
+++ ctrack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4y=sydmh)kb#lqyanw-x36prbj)(($a)zhpuk(%dym=1njd83-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [.vercel.app]
+ALLOWED_HOSTS = [*.vercel.app]
 
 
 # Application definition
