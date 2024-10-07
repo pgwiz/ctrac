6cf5be2f1b5a7daa65120abc4f7a35520a5f3b5a
Peter Brian Gwadenya <75813303+pgwiz@users.noreply.github.com>
2024-10-07 23:33:11 +0300

    Update settings.py

--- ctrack/settings.py
+++ ctrack/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
     # Add other allowed hosts here, if needed
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 STATICFILES_DIRS = [
     BASE_DIR / "car_tracker_app/templates/src",
