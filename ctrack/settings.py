3432b7890bc38a45ba6128907372af8226c3f7fc
Peter Brian Gwadenya <75813303+pgwiz@users.noreply.github.com>
2024-10-09 00:42:00 +0300

    Update settings.py

--- ctrack/settings.py
+++ ctrack/settings.py
@@ -41,7 +41,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'car_tracker_app', 'media')
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "car_tracker_app/templates/src",
@@ -157,7 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/car_tracker_app/templates/src/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
